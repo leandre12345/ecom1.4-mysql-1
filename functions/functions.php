403b322292bc03a9b52d31e0953208d44ca06bc1
Julian <dussollier.julian@gmail.com>
2023-10-31 14:35:38 -0400

    save functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -74,79 +74,7 @@ function getAllUsersIndex()
     return $data;
 }
 
-function createUser($data)
-{
-    global $conn;
-    /**
-     * Création du'une requete SQL préparée 
-     * en vue  d'une insertion 
-     */
-    $query = "INSERT INTO user VALUES (NULL, ?, ?, ?)";
 
-    /**
-     * utilisation de la fonction php mysqli_prepare() 
-     * pour préparer la requete et créer le statement.
-     * Cela vérifie que la connexion est bonne 
-     * et que la requete est valide sur la DB utilisée
-     */
-    if ($stmt = mysqli_prepare($conn, $query)) {
-        /** Lecture des marqueurs (les 3 "?" dans la query)
-         * Et on bind les param 
-         * en indiquant leur type ( s = string, i = integer)
-         * en donnant la valeur des param a inserer dans la DB ($data[key])
-         */
-        mysqli_stmt_bind_param(
-            $stmt,
-            "sss",
-            $data['user_name'],
-            $data['email'],
-            $data['pwd']
-        );
 
-        /* Exécution de la requête */
-        $result = mysqli_stmt_execute($stmt);
-    }
-}
 
-function updateUser($data)
-{
-    global $conn;
 
-    $query = "UPDATE user SET user_name = ?, email = ?, pwd = ?
-            WHERE user.id = ?;";
-
-    if ($stmt = mysqli_prepare($conn, $query)) {
-
-        mysqli_stmt_bind_param(
-            $stmt,
-            "sssi",
-            $data['user_name'],
-            $data['email'],
-            $data['pwd'],
-            $data['id'],
-        );
-
-        /* Exécution de la requête */
-        $result = mysqli_stmt_execute($stmt);
-    }
-}
-
-function deleteUser($id)
-{
-    global $conn;
-
-    $query = "DELETE FROM user
-            WHERE user.id = ?;";
-
-    if ($stmt = mysqli_prepare($conn, $query)) {
-
-        mysqli_stmt_bind_param(
-            $stmt,
-            "i",
-            $id,
-        );
-
-        /* Exécution de la requête */
-        $result = mysqli_stmt_execute($stmt);
-    }
-}
